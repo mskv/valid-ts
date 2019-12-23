e10880813a5af5f390922b9d2d6a1249b6c0d1db
Michał Podwórny <podworny.m@gmail.com>
2019-12-23 13:22:44 +0100

    bump libs

--- src/validators/optional.ts
+++ src/validators/optional.ts
@@ -3,4 +3,6 @@ import { Result } from "../result";
 import { validator, Validator } from "./validator";
 
 export const optional = <I, O, E>(inner: Validator<I, O, E>) =>
-  validator<I | undefined, O | undefined, E>((input) => input === undefined ? Result.ok(input) : inner(input));
+  validator<I | undefined, O | undefined, E>(input =>
+    input === undefined ? Result.ok(input as undefined) : inner(input)
+  );
